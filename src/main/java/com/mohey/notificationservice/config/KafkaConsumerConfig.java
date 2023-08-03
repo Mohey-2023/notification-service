bda1d4bbe3ffb76e936c0a4d752f399958ec0beb
yerimstar <yerimhuh@naver.com>
2023-08-04 00:22:30 +0900

    :sparkles: feat(consumer-config): Kafka Listener Container Factory 추가

--- src/main/java/com/mohey/notificationservice/config/KafkaConsumerConfig.java
+++ src/main/java/com/mohey/notificationservice/config/KafkaConsumerConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
@@ -23,5 +24,11 @@ public class KafkaConsumerConfig {
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(properties);
     }
-
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String,String> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String,String> kafkaListenerContainerFactory
+                = new ConcurrentKafkaListenerContainerFactory<>();
+        kafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
+        return kafkaListenerContainerFactory;
+    }
 }
