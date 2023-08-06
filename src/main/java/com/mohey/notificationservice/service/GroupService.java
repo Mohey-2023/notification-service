5fdc83c93897de871018f46179d5a84bb4f66a5b
yerimstar <yerimhuh@naver.com>
2023-08-06 23:50:15 +0900

    :pencil2: fix(noti-group): user 정보 반복문 처리

--- src/main/java/com/mohey/notificationservice/service/GroupService.java
+++ src/main/java/com/mohey/notificationservice/service/GroupService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mohey.notificationservice.document.NotificationDocument;
 import com.mohey.notificationservice.dto.BaseNotificationDto;
+import com.mohey.notificationservice.dto.UserNotificationDetailDto;
 import com.mohey.notificationservice.repository.NotificationRepo;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,16 +21,18 @@ public class GroupService {
         try{
             BaseNotificationDto baseNotificationDto = mapper.readValue(kafkaMessage,BaseNotificationDto.class);
             log.info("baseNoti : " + baseNotificationDto);
-            NotificationDocument document = NotificationDocument.builder()
-                    .topic(baseNotificationDto.getTopic())
-                    .type(baseNotificationDto.getType())
-                    .receiverUuid(baseNotificationDto.getReceiverUuid())
-                    .receiverName(baseNotificationDto.getReceiverName())
-                    .senderName(baseNotificationDto.getSenderName())
-                    .groupInfoDto(baseNotificationDto.getGroupNotificationDto())
-                    .build();
-            log.info("document = " + document);
-            notificationRepo.save(document);
+            for(UserNotificationDetailDto userNotificationDetailDto: baseNotificationDto.getUserNotificationDetailDtoList()) {
+                NotificationDocument document = NotificationDocument.builder()
+                        .topic(baseNotificationDto.getTopic())
+                        .type(baseNotificationDto.getType())
+                        .receiverUuid(userNotificationDetailDto.getReceiverUuid())
+                        .receiverName(userNotificationDetailDto.getReceiverName())
+                        .senderName(baseNotificationDto.getSenderName())
+                        .groupInfoDto(baseNotificationDto.getGroupNotificationDto())
+                        .build();
+                log.info("document = " + document);
+                notificationRepo.save(document);
+            }
         }catch (JsonProcessingException ex){
             ex.printStackTrace();
         }
