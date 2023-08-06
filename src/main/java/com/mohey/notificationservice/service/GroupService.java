68371c49bee411b0d9fef32647e4b4c0fb9509b6
yerimstar <yerimhuh@naver.com>
2023-08-06 16:31:28 +0900

    :sparkles: feat(noti-group): GroupService 추가 - MongoDB

--- src/main/java/com/mohey/notificationservice/service/GroupService.java
+++ src/main/java/com/mohey/notificationservice/service/GroupService.java
@@ -1,7 +1,38 @@
 package com.mohey.notificationservice.service;
 
-public class GroupService {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mohey.notificationservice.document.NotificationDocument;
+import com.mohey.notificationservice.dto.BaseNotificationDto;
+import com.mohey.notificationservice.repository.NotificationRepo;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
-    public void insertGroupNoti(String kafkaMessage) {
+@Service
+@AllArgsConstructor
+@Slf4j
+public class GroupService {
+    private NotificationRepo notificationRepo;
+
+    public void insertGroupNoti(String kafkaMessage){
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            BaseNotificationDto baseNotificationDto = mapper.readValue(kafkaMessage,BaseNotificationDto.class);
+            log.info("baseNoti : " + baseNotificationDto);
+            NotificationDocument document = NotificationDocument.builder()
+                    .topic(baseNotificationDto.getTopic())
+                    .type(baseNotificationDto.getType())
+                    .receiverUuid(baseNotificationDto.getReceiverUuid())
+                    .receiverName(baseNotificationDto.getReceiverName())
+                    .senderName(baseNotificationDto.getSenderName())
+                    .groupInfoDto(baseNotificationDto.getGroupNotificationDto())
+                    .build();
+            log.info("document = " + document);
+            notificationRepo.save(document);
+        }catch (JsonProcessingException ex){
+            ex.printStackTrace();
+        }
     }
+
 }
