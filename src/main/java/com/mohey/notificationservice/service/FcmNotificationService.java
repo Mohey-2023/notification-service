47c5bde82076b5c7942880575d7ea12efbbc8c8e
yerimstar <yerimhuh@naver.com>
2023-08-07 14:29:02 +0900

    :pencil2: fix(noti-friend): BaseNotificationDto를 MemberNotificationDto로 변경, senderPersonal를 member와 group으로 분리

--- src/main/java/com/mohey/notificationservice/service/FcmNotificationService.java
+++ src/main/java/com/mohey/notificationservice/service/FcmNotificationService.java
@@ -3,9 +3,10 @@ package com.mohey.notificationservice.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mohey.notificationservice.dto.BaseNotificationDto;
+import com.mohey.notificationservice.dto.GroupNotificationDto;
 import com.mohey.notificationservice.dto.FCMNotificationDto;
-import com.mohey.notificationservice.dto.UserNotificationDetailDto;
+import com.mohey.notificationservice.dto.MemberNotificationDetailDto;
+import com.mohey.notificationservice.dto.MemberNotificationDto;
 import com.mohey.notificationservice.producer.PersonalProducer;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.lang.reflect.Member;
 import java.util.Map;
 
 @Service
@@ -23,22 +25,45 @@ public class FcmNotificationService {
     private PersonalProducer personalProducer;
     private  ObjectMapper mapper;
 
-    public void sendPersonal(String kafkaMessage) throws IOException {
+    public void sendMemberPersonal(String kafkaMessage) throws IOException {
         mapper = new ObjectMapper();
         Map<String, Map<String, String>> templates = loadTemplates(mapper);
         try{
-            BaseNotificationDto baseNotificationDto = mapper.readValue(kafkaMessage,BaseNotificationDto.class);
-            log.info("baseNotificationDto = " + baseNotificationDto);
-            String topic = baseNotificationDto.getTopic();
+            MemberNotificationDto memberNotificationDto = mapper.readValue(kafkaMessage, MemberNotificationDto.class);
+            log.info("memberNotificationDto = " + memberNotificationDto);
+            String topic = memberNotificationDto.getTopic();
             Map<String, String> template = templates.get(topic);
             String title = template.get("title");
             String body = template.get("body")
-                    .replace("{senderName}", baseNotificationDto.getSenderName());
-            if(baseNotificationDto.getGroupNotificationDto() != null){
-                 body = body.replace("{groupName}",baseNotificationDto.getGroupNotificationDto().getGroupName());
+                    .replace("{senderName}", memberNotificationDto.getSenderName());
+
+            for (String fcmToken : memberNotificationDto.getDeviceTokenList()) {
+                log.info("fcmToken : " + fcmToken);
+                FCMNotificationDto fcmNotificationDto =
+                        new FCMNotificationDto(fcmToken,title, body);
+                log.info("fcmNotiDto : " + fcmNotificationDto);
+                personalProducer.send("personal-push", fcmNotificationDto);
             }
-            for(UserNotificationDetailDto userNotificationDetailDto: baseNotificationDto.getUserNotificationDetailDtoList()){
-                for (String fcmToken : userNotificationDetailDto.getDeviceTokenList()) {
+        }catch (JsonProcessingException ex){
+            ex.printStackTrace();
+        }
+    }
+    public void sendGroupPersonal(String kafkaMessage) throws IOException {
+        mapper = new ObjectMapper();
+        Map<String, Map<String, String>> templates = loadTemplates(mapper);
+        try{
+            GroupNotificationDto groupNotificationDto = mapper.readValue(kafkaMessage, GroupNotificationDto.class);
+            log.info("baseNotificationDto = " + groupNotificationDto);
+            String topic = groupNotificationDto.getTopic();
+            Map<String, String> template = templates.get(topic);
+            String title = template.get("title");
+            String body = template.get("body")
+                    .replace("{senderName}", groupNotificationDto.getSenderName());
+            if(groupNotificationDto.getGroupNotificationDetailDto() != null){
+                 body = body.replace("{groupName}", groupNotificationDto.getGroupNotificationDetailDto().getGroupName());
+            }
+            for(MemberNotificationDetailDto memberNotificationDetailDto : groupNotificationDto.getMemberNotificationDetailDtoList()){
+                for (String fcmToken : memberNotificationDetailDto.getDeviceTokenList()) {
                     log.info("fcmToken : " + fcmToken);
                     FCMNotificationDto fcmNotificationDto =
                             new FCMNotificationDto(fcmToken,title, body);
