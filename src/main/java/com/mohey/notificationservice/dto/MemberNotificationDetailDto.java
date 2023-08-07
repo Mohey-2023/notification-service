5e783e02b0e0b0c42b345617b2486f7c668ab51d
yerimstar <yerimhuh@naver.com>
2023-08-07 15:08:56 +0900

    :pencil2: fix(noti-friend): UserNotificationDetailDto를 MemberNotificationDetailDto로 이름 변경

--- src/main/java/com/mohey/notificationservice/dto/MemberNotificationDetailDto.java
+++ src/main/java/com/mohey/notificationservice/dto/MemberNotificationDetailDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-public class UserNotificationDetailDto {
+public class MemberNotificationDetailDto {
     private String receiverUuid;
     private String receiverName;
     private List<String> deviceTokenList;
