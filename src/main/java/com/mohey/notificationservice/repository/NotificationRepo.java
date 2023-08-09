3013a7d92725516db976847e2c3cb6274bf02bc4
yerimstar <yerimhuh@naver.com>
2023-08-10 00:15:51 +0900

    :zap: feat(fetch-notifications): 사용자 uuid 기반 최근 30일 알림 조회 추가

--- src/main/java/com/mohey/notificationservice/repository/NotificationRepo.java
+++ src/main/java/com/mohey/notificationservice/repository/NotificationRepo.java
@@ -4,5 +4,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.mohey.notificationservice.document.NotificationDocument;
 
+import java.util.Date;
+import java.util.List;
+
 public interface NotificationRepo extends MongoRepository<NotificationDocument,String> {
+    List<NotificationDocument> findByReceiverUuidAndTypeAndCreatedTimeAfter(String memberUuid, String type, Date date);
 }
