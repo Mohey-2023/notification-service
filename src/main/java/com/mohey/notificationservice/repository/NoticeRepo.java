85189c133557c10b54d22eaf5920f29ed2ad5107
yerimstar <yerimhuh@naver.com>
2023-08-10 00:15:06 +0900

    :zap: feat(fetch-notifications): 최근 30일 공지 조회 추가

--- src/main/java/com/mohey/notificationservice/repository/NoticeRepo.java
+++ src/main/java/com/mohey/notificationservice/repository/NoticeRepo.java
@@ -3,5 +3,9 @@ package com.mohey.notificationservice.repository;
 import com.mohey.notificationservice.document.NoticeDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface NoticeRepo extends MongoRepository<NoticeDocument,String> {
+    List<NoticeDocument> findByCreatedTimeAfter(Date date);
 }
